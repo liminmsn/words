da56f075d2d4b30da1661c78241f7d78524fc5d5
杨再涛 <1772081518@qq.com>
2025-02-08 15:06:32 +0800

    更新

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,8 @@
+import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MaterialApp(
-    title: 'My app',
-    home: SafeArea(child: MyApp()),
-  ));
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -12,49 +10,23 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Material(
-      child: Column(
-        children: [
-          MyAppBar(
-            title: Text('Example title',
-                style: Theme.of(context).primaryTextTheme.titleLarge),
-          ),
-          const Expanded(
-            child: Center(
-              child: Text('Hello, world!'),
+    return MaterialApp(
+      home: Scaffold(
+        body: Column(
+          children: [
+            Text("hello world"),
+            Expanded(
+              child: ListView(
+                children: [
+                  for (var item in nouns.take(10))
+                    ListTile(
+                      title: Text(item),
+                    ),
+                ],
+              ),
             ),
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class MyAppBar extends StatelessWidget {
-  const MyAppBar({super.key, required this.title});
-
-  final Widget title;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      height: 56,
-      padding: const EdgeInsets.symmetric(horizontal: 8),
-      decoration: BoxDecoration(color: Colors.yellow[500]),
-      child: Row(
-        children: [
-          const IconButton(
-            tooltip: 'Navigation menu',
-            icon: Icon(Icons.menu),
-            onPressed: null,
-          ),
-          Expanded(child: Text('hello')),
-          const IconButton(
-            icon: Icon(Icons.search),
-            tooltip: 'Search',
-            onPressed: null,
-          )
-        ],
+          ],
+        ),
       ),
     );
   }
