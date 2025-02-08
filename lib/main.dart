157ecb3374a4f2619a9353e26ecbd08e30c34067
杨再涛 <1772081518@qq.com>
2025-02-08 16:02:19 +0800

    layout

--- lib/main.dart
+++ lib/main.dart
@@ -12,21 +12,49 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: Column(
-          children: [
-            Text("hello world"),
-            Expanded(
-              child: ListView(
-                children: [
-                  for (var item in nouns.take(10))
-                    ListTile(
-                      title: Text(item),
-                    ),
-                ],
-              ),
-            ),
-          ],
+        body: Expanded(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              for (var i = 0; i < 3; i++)
+                Expanded(
+                  child: Column(
+                    children: [
+                      BorderedImage(),
+                      Text(i.toString()),
+                    ],
+                  ),
+                ),
+            ],
+          ),
         ),
+      ),
+    );
+  }
+}
+
+class BorderedImage extends StatelessWidget {
+  const BorderedImage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.all(
+          Radius.circular(12),
+        ),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withAlpha(100),
+            offset: Offset(1, 1),
+          ),
+          BoxShadow(color: Colors.white),
+        ],
+      ),
+      padding: EdgeInsets.all(10),
+      child: FlutterLogo(
+        size: 100,
       ),
     );
   }
