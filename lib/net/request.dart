47855547536cb2844b409f7fdf58843d2723069e
杨再涛 <1772081518@qq.com>
2025-02-16 18:44:40 +0800

    更新

---
+++ lib/net/request.dart
@@ -0,0 +1,22 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
+typedef RequCall = void Function(String dom);
+
+class YRequest {
+  static final String url = "https://fulitu.neocities.org";
+  late String urla = url;
+  String? urlb;
+  YRequest([this.urlb]) : urla = url + (urlb ?? '');
+  void get(RequCall r) async {
+    late Uri uri = Uri.parse(urla);
+    var res = await http.get(uri);
+    if (res.statusCode == 200) {
+      String htmlbody = utf8.decode(res.bodyBytes);
+      r.call(htmlbody);
+    } else {
+      r.call("<html><body></body></html>");
+    }
+  }
+}
--- lib/view/view_home.dart
+++ lib/view/view_home.dart
@@ -1,18 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:words/net/request.dart';
+import 'dart:convert';
 
 class ViewHome extends StatefulWidget {
   const ViewHome({super.key});
-
   @override
   State<ViewHome> createState() => _ViewHomeState();
 }
 
 class _ViewHomeState extends State<ViewHome> {
+  String body = "none";
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Column(
-        children: [Text("Home")],
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            Text(body),
+            ElevatedButton(
+              onPressed: () {
+                YRequest().get((body) {
+                  setState(() {
+                    this.body = body;
+                  });
+                });
+              },
+              child: Text("Get"),
+            ),
+          ],
+        ),
       ),
     );
   }
