34aee224a5fff78e1429d88e57cdfab0702befdb
杨再涛 <1772081518@qq.com>
2025-02-19 21:56:50 +0800

    更新

--- android/app/src/main/kotlin/com/example/words/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/words/MainActivity.kt
@@ -1,5 +1,11 @@
 package com.example.words
 
+import android.content.Context
+import android.content.ContextParams
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity()
+class MainActivity: FlutterActivity(){
+    MainActivity(){
+        
+    }
+}
