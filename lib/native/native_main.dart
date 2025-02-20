e2cd3e22c3e60f784f815de3be5c108c9e6812a0
杨再涛 <1772081518@qq.com>
2025-02-20 12:11:02 +0800

    test

--- lib/native/native_main.dart
+++ lib/native/native_main.dart
@@ -4,7 +4,7 @@ class NativeMain {
   static const platform = MethodChannel('com.example.words.native/uuid');
 
   static Future<String> get uuid async {
-    final String version = await platform.invokeMethod('getPlatformVersion');
+    final String version = await platform.invokeMethod('uuid');
     return version;
   }
 }
