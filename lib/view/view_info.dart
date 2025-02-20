128a6a42fd7b5528e08dd27edce67fb6cc4f5ca9
杨再涛 <1772081518@qq.com>
2025-02-20 12:05:13 +0800

    uuid

--- lib/view/view_info.dart
+++ lib/view/view_info.dart
@@ -17,7 +17,13 @@ class _ViewInfoState extends State<ViewInfo> {
       child: Center(
         child: Column(
           children: [
-            Text('UID: $_uid'),
+            Container(
+              padding: EdgeInsets.all(20),
+              child: Text(
+                "UID: $_uid",
+                style: TextStyle(fontSize: 20),
+              ),
+            ),
             ElevatedButton(
               onPressed: () async {
                 var a = await NativeMain.uuid;
