a7b812201aa5437395b5ddf3da05a1c29a3f3849
杨再涛 <1772081518@qq.com>
2025-02-22 17:52:33 +0800

    reset

--- lib/main.dart
+++ lib/main.dart
@@ -11,28 +11,14 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  var selectIndex = 0;
-  late List<Widget> pageArr;
-
-  @override
-  void initState() {
-    super.initState();
-    pageArr = [Home(callback: setSelectIndex), ScaffoldTest()];
-  }
-
-  void setSelectIndex(int idx) {
-    setState(() {
-      selectIndex = idx;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Namer App',
       theme: ThemeData(
-          useMaterial3: true,
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple)),
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.red),
+      ),
       home: Scaffold(
         appBar: AppBar(
           backgroundColor: Theme.of(context).colorScheme.onPrimaryContainer,
@@ -41,76 +27,11 @@ class _MyAppState extends State<MyApp> {
             style: TextStyle(color: Colors.white),
           ),
         ),
-        body: pageArr[selectIndex],
-      ),
-    );
-  }
-}
-
-class Home extends StatelessWidget {
-  final Function(int) callback;
-  const Home({super.key, required this.callback});
-
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Builder(builder: (context) {
-        return Column(
-          children: [
-            Container(
-              decoration: BoxDecoration(
-                shape: BoxShape.circle,
-                color: Colors.amber,
-              ),
-              padding: EdgeInsets.all(10),
-              margin: EdgeInsets.fromLTRB(0, 20, 0, 20),
-              child: Text('Hello, Flutter!'),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                callback(1);
-              },
-              child: Text("A button"),
-            ),
-          ],
-        );
-      }),
-    );
-  }
-}
-
-class ScaffoldTest extends StatefulWidget {
-  const ScaffoldTest({super.key});
-
-  @override
-  State<ScaffoldTest> createState() => _ScaffoldTestState();
-}
-
-class _ScaffoldTestState extends State<ScaffoldTest> {
-  int _count = 0;
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Sample Code"),
-      ),
-      body: Center(
-        child: Text('You have pressed the button $_count times.'),
-      ),
-      bottomNavigationBar: BottomAppBar(
-        shape: const CircularNotchedRectangle(),
-        child: Container(
-          height: 50,
+        body: SafeArea(
+          child: Column(
+            children: [],
+          ),
         ),
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-      floatingActionButton: FloatingActionButton(
-        onPressed: () => setState(() {
-          _count++;
-        }),
-        tooltip: 'Increment Counter',
-        child: const Icon(Icons.add),
       ),
     );
   }
